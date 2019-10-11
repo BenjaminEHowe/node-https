ac696e771445705faa8d3133356b8f03c2c28be2
Benjamin Howe <ben@bh96.uk>
2019-10-11 21:14:42 +0100

    Fixed issue by adding dhparams

--- generate-https.sh
+++ generate-https.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+openssl dhparam -out dhparam.pem 2048
 openssl req -new -newkey rsa:2048 -days 365 -nodes -x509 -subj "/C=GB/ST=West Sussex/L=Haywards Heath/O=Benjamin Howe/CN=127.0.0.1" -keyout 127-0-0-1.key -out 127-0-0-1.cert
--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-const express = require('express');
 const fs = require('fs');
 const https = require('https');
 
-const app = express();
-
 let key = fs.readFileSync('127-0-0-1.key', 'ascii');
 let cert = fs.readFileSync('127-0-0-1.cert', 'ascii');
+let dhparam = fs.readFileSync('dhparam.pem', 'ascii');
 
 https.createServer(
   {
     key,
     cert,
+    dhparam,
     ciphers:'ECDHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES128-GCM-SHA256'
   },
-  app
+  (req, res) => { res.end(); }
 ).listen(8443, '127.0.0.1');
